3ac84030a9ef8af464ef0c1ceb30a9e3c1bc0771
Luci0 <luci00@gmail.com>
2025-10-20 16:50:11 +0300

    fix: user permissions chmod

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,7 @@ docker-compose down torrent-client
 # Remove the existing image
 docker rmi $(docker images -q --filter "reference=shitflix-v2-torrent-client*")
 
+#Remove the volumes
+docker volume rm shitflix-v2_transmission-config
 # Rebuild and start the service
-docker-compose up --build torrent-client --force-recreate --renew-anon-volumes
+docker-compose up --build torrent-client --force-recreate
