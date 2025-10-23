999ea9312f14da4dbbfab2c147780a700f744cad
Luci0 <luci00@gmail.com>
2025-10-23 11:40:52 +0300

    fix: run generate wishlist

--- scripts/shitflix-runner.sh
+++ scripts/shitflix-runner.sh
@@ -2,6 +2,6 @@
 
 script_dir=$(realpath "$(dirname -- "$0")")
 
-#"$script_dir/generate-wishlist.sh"
+"$script_dir/generate-wishlist.sh"
 
 "$script_dir/wishlist-processor.sh" "$script_dir/txts/wishlist.txt"
